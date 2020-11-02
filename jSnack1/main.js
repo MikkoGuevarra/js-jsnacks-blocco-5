cf2cf0ebc2e33856d3c1b95206fa2181ceedcd3c
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-02 16:35:15 +0100

    tried but not functioning

--- jSnack1/main.js
+++ jSnack1/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 // JSnack1: Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
+//creo oggetti bici
 var bike = [
     {
         'nome' : 'Shava',
@@ -17,16 +18,24 @@ var bike = [
 ];
 console.log(bike);
 
+//creo variabile per il peso corrente che sto scorrendo
+var currentWeight;
+
+//creo variabile per salvare il peso precedente
+var minor;
+
+//cre ciclo per scorrere ogni oggetto dell'array
 for (var i = 0; i < bike.length; i++) {
     thisBike = bike[i]
     console.log(thisBike);
+    //creo ciclo for in per stampare ogni singola proprietà dell'oggetto
     for (var key in bike[i]) {
-        var currentWeight = thisBike['peso'];
+        currentWeight = thisBike['peso'];
         console.log( thisBike['nome'] + ' ha un peso' + ' ' + currentWeight);
         // console.log(key + ': ' + thisBike[key]);
-        if (thisBike['peso'] < currentWeight) {
-            console.log(thisBike);
-
+        if (currentWeight < minor) {
+            minor = currentWeight;
+            console.log('winner ' + bike[i]);
         }
     }
 }
